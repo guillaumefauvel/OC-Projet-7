57cc2698cf72a9553c463baf4ddfc10c101fefcc
Guillaume Fauvel <89651835+guillaumefauvel@users.noreply.github.com>
2022-01-06 11:56:07 +0100

    Modification de la fonction to_int

--- optimized.py
+++ optimized.py
@@ -1,7 +1,6 @@
 import numpy as np
 from datetime import datetime
-import csv
-
+import math
 
 CAPITAL = 500
 
@@ -16,19 +15,39 @@ def stocks_dict(csv_file):
         for line in csv:
             listed_line = line.split(",")
             listed_line[2] = listed_line[2].split('\n')[0]
-
             try:
                 cost = float(listed_line[1])
                 performance = float(listed_line[2])
-                added_performance = round(cost*performance/100,3)
-                name = listed_line[0]
-                stock_dict[index] = cost, added_performance, name
-                index += 1
+                if cost > 0:
+                    added_performance = round(cost*performance/100,3)
+                    name = listed_line[0]
+                    stock_dict[index] = cost, added_performance, name
+                    index += 1
 
             except ValueError:
+
                 pass
 
     return stock_dict
+
+def to_int(array):
+
+    longest_frac = 0
+
+    for value in array:
+        frac, whole = math.modf(value)
+        whole_len = len(str(int(whole)))
+        frac_len = len(str(value).replace('.',''))-whole_len
+        if frac_len > longest_frac:
+            longest_frac = frac_len
+
+    adjusted_array = []
+
+    for value in array:
+        adjusted_array.append(round(value * 10 ** longest_frac))
+
+    return adjusted_array, longest_frac
+
 
 def common_step(dict_of_stocks):
     # Get the greatest common divisor in order to reduce the table
@@ -37,18 +56,7 @@ def common_step(dict_of_stocks):
 
     formated_dict = [dict_of_stocks[a][0] for a in dict_of_stocks]
 
-    def to_int(array, coef):
-        signal = True
-        for value in array:
-            if not value.is_integer():
-
-                return to_int(np.array(array)*10, coef+1)
-        if signal is True:
-            reference_list = [int(a) for a in array]
-
-            return [reference_list, coef]
-
-    adjusted_array, coef = to_int(formated_dict, 0)
+    adjusted_array, coef = to_int(formated_dict)
     gcd = np.gcd.reduce(adjusted_array)/(10**coef)
 
     return gcd
@@ -77,7 +85,10 @@ def optimized_algo(stocks_dict):
                     added_stock_name = stocks_dict[stock][2]
 
                     without_last = table[row - 1][column - int(stocks_dict[stock][0] / step)][0]
-                    without_last_stock = table[row - 1][column - int(stocks_dict[stock][0] / step)][1]
+                    try:
+                        without_last_stock = table[row - 1][column - int(stocks_dict[stock][0] / step)][1]
+                    except:
+                        pass
 
                     if (added_value + without_last) > above_cell_perf:
                         table[row][column][0] = added_value + without_last
@@ -100,11 +111,18 @@ def optimized_algo(stocks_dict):
     added_value, combinations = best_cell
     return_on_investment = round(added_value / CAPITAL * 100,3)
 
-    print(f"\nLe meilleur ROI possible avec {CAPITAL} de crédit est de {return_on_investment}%."
-          f"\nIl est obtenu avec la combinaison suivante : {', '.join(combinations)}")
+    total_cost = 0
+    for stock in stocks_dict.values():
+        if stock[2] in combinations:
+            total_cost += stock[0]
+
+    print(f"\nLe meilleur ROI possible avec {CAPITAL} de crédit est de {return_on_investment}%, soit {added_value}e de gains.")
+    print(f"Le coût total est de {total_cost}e.")
+    print(f"\nIl est obtenu avec la combinaison suivante : {', '.join(combinations)}")
     print(f"\nL'opération a été effectué en {datetime.now()-start_time}")
 
 
-available_stocks = (stocks_dict("stocks.csv"))
+available_stocks = (stocks_dict("série2.csv"))
+
 optimized_algo(available_stocks)
 
