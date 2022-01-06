129cbdbecfc287519b5320a76443b6ea91765586
Guillaume Fauvel <89651835+guillaumefauvel@users.noreply.github.com>
2022-01-06 17:03:17 +0100

    Corrections - conditions

--- optimized.py
+++ optimized.py
@@ -4,7 +4,6 @@ import math
 
 CAPITAL = 500
 
-
 def stocks_dict(csv_file):
     """ Make a sorted stock dict out of a csv file
         Arg : A csv file, with containing three columns, 1:reference, 2:cost, 3:stock_performance
@@ -25,12 +24,14 @@ def stocks_dict(csv_file):
                     index += 1
 
             except ValueError:
-
                 pass
 
     return stock_dict
 
 def to_int(array):
+    """ Convert an array of number into integers with multiplication factor
+    Arg : An array of number ( float, usually )
+    Returns : An array of integers, the multiplication factor """
 
     longest_frac = 0
 
@@ -50,9 +51,9 @@ def to_int(array):
 
 
 def common_step(dict_of_stocks):
-    # Get the greatest common divisor in order to reduce the table
-    # Arg : A dictionnary of stocks where the first value correspond to the cost
-    # Return : An int, the GCD
+    """ Get the greatest common divisor in order to reduce the table
+    Arg : A dictionnary of stocks where the first value correspond to the cost
+    Return : An int, the GCD """
 
     formated_dict = [dict_of_stocks[a][0] for a in dict_of_stocks]
 
@@ -62,67 +63,72 @@ def common_step(dict_of_stocks):
     return gcd
 
 
-def optimized_algo(stocks_dict):
-
+def optimized_algo(stocks_dict, max_credit):
+    """ Create a grid in order to have the best possible value for money
+    Arg : A dictionnary of stocks ( key=index, v1=cost, v2=added_value, v3=stocks_reference ) """
     start_time = datetime.now()
 
     step = common_step(stocks_dict)
-    number_of_steps = int(CAPITAL/step)
-    table = [[[0, []] for index in range(number_of_steps+1)] for value in range(len(stocks_dict))]
+    number_of_steps = int(max_credit/step)
+    print(len(stocks_dict), number_of_steps+1)
+    table = [[[0, [], 0] for index in range(number_of_steps+1)] for value in range(len(stocks_dict))]
 
     for stock, row in zip(stocks_dict, range(0, len(stocks_dict))):
-
+        print(stock)
         for column in range(len(table[row])):
 
             above_cell_perf = table[row - 1][column][0]
-            above_cell_stocks = table[row - 1][column][1]
 
             if stocks_dict[stock][0] <= column*step:
 
                 if column*step - stocks_dict[stock][0] >= 0:
 
+                    added_stock_cost = stocks_dict[stock][0]
                     added_value = stocks_dict[stock][1]
                     added_stock_name = stocks_dict[stock][2]
 
-                    without_last = table[row - 1][column - int(stocks_dict[stock][0] / step)][0]
-                    try:
-                        without_last_stock = table[row - 1][column - int(stocks_dict[stock][0] / step)][1]
-                    except:
-                        pass
+                    without_last = table[row - 1][column - math.floor(stocks_dict[stock][0] / step)][0]
+                    without_last_stock = table[row - 1][column - math.floor(stocks_dict[stock][0] / step)][1]
+                    without_last_cost = table[row - 1][column - math.floor(stocks_dict[stock][0] / step)][2]
 
-                    if (added_value + without_last) > above_cell_perf:
+                    if (added_value + without_last) > above_cell_perf and (added_stock_cost + without_last_cost) <= column*step:
                         table[row][column][0] = added_value + without_last
                         for stock_ref in without_last_stock:
                             table[row][column][1].append(stock_ref)
                         table[row][column][1].append(added_stock_name)
+                        table[row][column][2] = added_stock_cost + without_last_cost
 
                     else:
-                        table[row][column][0] = above_cell_perf
-                        table[row][column][1] = above_cell_stocks
+                        table[row][column] = table[row - 1][column]
 
                 else:
-                    table[row][column][0] = above_cell_perf
+                    table[row][column] = table[row - 1][column]
+
 
             else:
                 table[row][column] = table[row - 1][column]
 
 
     best_cell = table[len(stocks_dict)-1][number_of_steps]
-    added_value, combinations = best_cell
-    return_on_investment = round(added_value / CAPITAL * 100,3)
+    added_value, combinations, total_cost = best_cell
+    return_on_investment = round(added_value / max_credit * 100,3)
 
     total_cost = 0
     for stock in stocks_dict.values():
         if stock[2] in combinations:
             total_cost += stock[0]
 
-    print(f"\nLe meilleur ROI possible avec {CAPITAL} de crédit est de {return_on_investment}%, soit {added_value}e de gains.")
-    print(f"Le coût total est de {total_cost}e.")
-    print(f"\nIl est obtenu avec la combinaison suivante : {', '.join(combinations)}")
-    print(f"\nL'opération a été effectué en {datetime.now()-start_time}")
+    total_runtime = datetime.now()-start_time
+
+    return return_on_investment, added_value, total_cost, combinations, total_runtime
 
 
-available_stocks = (stocks_dict("série2.csv"))
+available_stocks = (stocks_dict("série1.csv"))
 
-optimized_algo(available_stocks)
+return_on_investment, added_value, total_cost, combinations, total_runtime = optimized_algo(available_stocks, CAPITAL)
 
+
+print(f"\nLe meilleur ROI possible avec {CAPITAL} de crédit est de {return_on_investment}%, soit {added_value}e de gains.")
+print(f"Le coût total est de {total_cost}e.")
+print(f"\nIl est obtenu avec la combinaison suivante : {', '.join(combinations)}")
+print(f"\nL'opération a été effectué en {total_runtime}")
